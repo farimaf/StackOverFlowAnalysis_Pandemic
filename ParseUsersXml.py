cfe240fe607514146ecf8902c7965ee835341a42
farimaf <farahani.farima@gmail.com>
2020-12-05 18:10:25 -0800

    read create dates more than 2017

--- ParseUsersXml.py
+++ ParseUsersXml.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from lxml import etree
+from datetime import datetime
 from xml.etree.ElementTree import XML, fromstring
 
 usersXmlPath=sys.argv[1]
@@ -13,20 +13,22 @@ headerRow="AccId,CreateDate,LastAccessDate,ViewsNum,UpVotes,DownVotes"
 
 for file in os.listdir(usersXmlPath):
     if file.endswith(".xml"):
+        print("processing "+file+" started")
         xmlFile = open(os.path.join(usersXmlPath,file), 'r', encoding='utf-8')
         usersCsvFilePath=os.path.join(usersCsvPath,file[0:len(file)-3]+"csv")
-        with open(usersCsvFilePath, 'a') as csvFile:
+        with open(usersCsvFilePath, 'w') as csvFile:
             csvFile.write(headerRow+"\n")
             for line in xmlFile:
                 if not line.lstrip().startswith("<row"):
                     continue
                 row = fromstring(line)
                 createDate = row.get('CreationDate')
-                lastAccessDate = row.get('LastAccessDate')
-                viewsNum = row.get('Views')
-                upVotes = row.get('UpVotes')
-                downVotes = row.get('DownVotes')
-                accId = row.get('AccountId')
-                rowCsv=accId+","+createDate+","+lastAccessDate+","+viewsNum+","+upVotes+","+downVotes
-                print(rowCsv)
-                csvFile.write(rowCsv+"\n")
+                if(datetime.strptime(createDate,'%Y-%m-%dT%H:%M:%S.%f')>datetime.strptime('2017-01-01' , '%Y-%m-%d')):
+                    lastAccessDate = row.get('LastAccessDate')
+                    viewsNum = row.get('Views')
+                    upVotes = row.get('UpVotes')
+                    downVotes = row.get('DownVotes')
+                    accId = row.get('AccountId')
+                    rowCsv=accId+","+createDate+","+lastAccessDate+","+viewsNum+","+upVotes+","+downVotes
+                    csvFile.write(rowCsv+"\n")
+        print("processing "+file+" finished")
